200b10e46c0635039c23506aa71127d89de5f3ea
Abdulfattox <uzkingshark@gmail.com>
2026-06-25 12:33:11 +0500

    Remove unused mini ERP code

--- src/core/production_map/compiler/normalize.rs
+++ src/core/production_map/compiler/normalize.rs
@@ -1,3 +1,4 @@
+#[cfg(test)]
 use std::collections::BTreeMap;
 
 use super::super::types::*;
--- src/core/worker_groups/store.rs
+++ src/core/worker_groups/store.rs
@@ -1,5 +1,6 @@
 use async_trait::async_trait;
 
+#[cfg(test)]
 use super::normalize::sort_groups;
 use super::{WorkerGroupError, WorkerGroupRecord, WorkerGroupStorePort};
 
